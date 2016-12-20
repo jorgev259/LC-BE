a66e849e316b83c26db1ae750a34ec3fa45f7c9a
rikumax25 <jorgev259@outlook.es>
2016-12-20 00:18:33 -0600

    Fixed error and formatting issues on link.cs

--- linkscloud/linkscloud/Models/link.cs
+++ linkscloud/linkscloud/Models/link.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -89,33 +90,27 @@ namespace linkscloud.Models
             this.desc = desc;
         }
 
-        public static void new_link()
+        public static void new_link(link data)
         {
+            var response = "";
+
             //starting conection 
             Coneccion cnx = new Coneccion();
             cnx.parametro();
             cnx.inicializa();
             String Consulta = "NEW_LINK";
             cnx.annadir_consulta(Consulta);
-            cnx.annadir_parametro("id_owner", this.id_owner);
-            cnx.annadir_parametro("title", this.title);
-            cnx.annadir_parametro("url", this.url);
-            cnx.annadir_parametro("desc", this.desc);
+            cnx.annadir_parametro("id_owner", data.id_owner);
+            cnx.annadir_parametro("title", data.title);
+            cnx.annadir_parametro("url", data.url);
+            cnx.annadir_parametro("desc", data.desc);
 
             MySqlDataReader Container;
             try
             {
                 Container = cnx.busca();
-                while (Container.Read())
-                {
-                    this.id = Convert.ToInt32(Container["id"]);
-                    this.id_owner = Convert.ToInt32(Container["id_owner"]);
-                    this.title = CONTENEDOR["title"].ToString();
-                    this.url = CONTENEDOR["url"].ToString();
-                    this.desc = CONTENEDOR["desc"].ToString();
-                }
-
                 Container.Close();
+                response = "Complete";
 
             } //If it fails it will capture the exception, e.Number returns the error number indetifier
             catch (MySqlException e)
