0eef42e8e4bbe8032db0f3f04cf8c367d88c7c50
doriandres <dorian.andres.cortes@gmail.com>
2016-12-19 22:25:53 -0600

    link class link controller

---
+++ linkscloud/linkscloud/Controllers/linkController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+using linkscloud.Models;
+using System.Web.Script.Serialization;
+
+namespace linkscloud.Controllers
+{
+    public class linkController : Controller
+    {
+        [HttpPost]
+        public String Post()
+        {
+            var Request = HttpContext.Current.Request;
+            var respone = "";
+
+            switch (Request["proc"])
+            {
+                case "new":
+                    link res = new link(Request["id_owner"], Request["title"], Request["url"], Request["desc"]);
+                    res.new_link();
+                    response = new JavaScriptSerializer().Serialize(res);
+                    break;
+
+                default:
+                    response = "Unknown Request";
+                    break;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ linkscloud/linkscloud/Models/link.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace linkscloud.Models
+{
+    public class link
+    {
+        //all the code here 
+
+
+        //private variables 
+        public int id;
+        public int id_owner;
+        public string title;
+        public string url;
+        public string desc;
+
+        //solving the pics problem to introduce later
+
+        /*public variables
+        public int id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public int id_owner
+        {
+            get
+            {
+                return _id_owner;
+            }
+            set
+            {
+                _id_owner = value;
+            }
+        }
+
+        public string title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+
+        public string url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                _url = value;
+            }
+        }
+
+        public string desc
+        {
+            get
+            {
+                return _desc;
+            }
+            set
+            {
+                _desc = value;
+            }
+        }
+        */
+
+        public link(int id_owner, string title, string url, string desc)
+        {
+            this.id_owner = id_owner;
+            this.title = title;
+            this.url = url;
+            this.desc = desc;
+        }
+
+        public static void new_link()
+        {
+            //starting conection 
+            Coneccion cnx = new Coneccion();
+            cnx.parametro();
+            cnx.inicializa();
+            String Consulta = "NEW_LINK";
+            cnx.annadir_consulta(Consulta);
+            cnx.annadir_parametro("id_owner", this.id_owner);
+            cnx.annadir_parametro("title", this.title);
+            cnx.annadir_parametro("url", this.url);
+            cnx.annadir_parametro("desc", this.desc);
+
+            MySqlDataReader Container;
+            try
+            {
+                Container = cnx.busca();
+                while (Container.Read())
+                {
+                    this.id = Convert.ToInt32(Container["id"]);
+                    this.id_owner = Convert.ToInt32(Container["id_owner"]);
+                    this.title = CONTENEDOR["title"].ToString();
+                    this.url = CONTENEDOR["url"].ToString();
+                    this.desc = CONTENEDOR["desc"].ToString();
+                }
+
+                Container.Close();
+
+            } //If it fails it will capture the exception, e.Number returns the error number indetifier
+            catch (MySqlException e)
+            {
+                switch (e.Number) //Add case for each e.Number
+                {
+                    case 1062: //Needs checking with e.Message to know if its email or username that's wrong
+                        var split = e.Message.Split(new string[] { "key " }, StringSplitOptions.None);
+                        response = split[split.Length - 1] + " already exists";
+                        break;
+                    default:
+                        response = "Unexpected Error, try again later";
+                        break;
+                }
+            }
+
+
+            cnx.conexion.Close();
+            cnx.conexion.Dispose();
+
+
+        } 
+
+
+
+
+
+    }
+}
